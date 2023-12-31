9aef2ed5e3e53db6b5f3f5217ea117e5ed948812
itstarun0511 <tarunchaudhary630@gmail.com>
2023-12-31 19:50:15 +0530

    recommit

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://technotes-api.onrender.com',
+    baseUrl: 'https://technotes-jbmt.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
